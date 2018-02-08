c292a3ccb5497534c93201daa66a3a2fb86945c2
Benjamin Butler <benjamin.butler@hutton.ac.uk>
2018-02-08 17:04:05 +0000

    Trying to avoid error mesage in powdR documentation.

--- R/powdR.R
+++ R/powdR.R
@@ -1,8 +1,7 @@
-#' powdR: A package for XRPD data analysis.
+#' powdR: A package for quantitative mineralogy.
 #'
 #' The powdR package provides a host of function relevant to XRPD data sets,
 #' including peak alignment and full pattern fitting.
-#'
 #' @docType package
 #' @name powdR
 NULL
