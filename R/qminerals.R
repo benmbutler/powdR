d08bdf0d811b159adb1d20d6dcd9dac6118a415a
Benjamin Butler <benjamin.butler@hutton.ac.uk>
2020-04-24 10:57:20 +0100

    CORUNDUM_MCCRONE removed from rockjock and qminerals updated

--- R/qminerals.R
+++ R/qminerals.R
@@ -1,37 +1,66 @@
 .qminerals <- function(x, xrd_lib) {
 
   #Make sure x is ordered if there are more than 1 phases in the library
-  if (length(x) > 1) {
-    x <- x[order(names(x))]
-  }
+  #if (length(x) > 1) {
+  #  x <- x[order(names(x))]
+  #}
 
-  #Restrict the xrd library to phases within the names of fpf_pc
+  #Restrict the xrd library to phases within the names of x
   minerals <- xrd_lib$phases
 
   minerals <- minerals[which(minerals$phase_id %in% names(x)),]
 
-  #Order to the same as fpf_pc
-  if (length(x) > 1) {
-    minerals <- minerals[order(minerals$phase_id),]
+  #make sure the minerals data frame is in the same order as x
+  minerals <- minerals[match(names(x), minerals$phase_id),]
+
+  if(!identical(names(x), minerals$phase_id)) {
+
+    stop("The phase ID's for quantification do not match the names of the scaling coefficients.")
+
   }
 
-  min_percent <- (x/minerals$rir)/sum(x/minerals$rir)*100
+  #if !(identical(minerals$phase_id, names x)) {
+  #
+   # stop("The names ")
 
-  names(min_percent) <- minerals$phase_id
+  #}
 
-  df <- data.frame(minerals, "phase_percent" = min_percent)
-  row.names(df) = c(1:nrow(df))
+  #Order to the same as fpf_pc
+  #if (length(x) > 1) {
+  #  minerals <- minerals[order(minerals$phase_id),]
+  #}
+
+  #min_percent <- (x/minerals$rir)/sum(x/minerals$rir)*100
+
+  #names(min_percent) == minerals$phase_id
+
+  minerals$phase_percent <- (x/minerals$rir)/sum(x/minerals$rir)*100
+
+  #df <- data.frame(minerals, "phase_percent" = min_percent)
+  row.names(minerals) = c(1:nrow(minerals))
+
+  minerals$phase_order <- c(1:nrow(minerals))
 
   #Summarise by summing the concentrations from each mineral group
 
-  dfs <- data.frame(stats::aggregate(phase_percent ~ phase_name, data = df, FUN = sum),
+  minerals_g <- data.frame(stats::aggregate(phase_percent ~ phase_name, data = minerals, FUN = sum),
                     stringsAsFactors = FALSE)
 
-  #Ensure that the phase concentrations are rounded to 4 dp
-  df$phase_percent <- round(df$phase_percent, 4)
-  dfs$phase_percent <- round(dfs$phase_percent, 4)
+  minerals_o <- data.frame(stats::aggregate(phase_order ~ phase_name, data = minerals, FUN = mean),
+                           stringsAsFactors = FALSE)
 
-  out <- list("df" = df, "dfs" = dfs)
+  minerals_g <- plyr::join(minerals_g, minerals_o, by = "phase_name")
+
+  minerals_g <- minerals_g[order(minerals_g$phase_order),]
+
+  minerals$phase_order <- NULL
+  minerals_g$phase_order <- NULL
+
+  #Ensure that the phase concentrations are rounded to 4 dp
+  minerals$phase_percent <- round(minerals$phase_percent, 4)
+  minerals_g$phase_percent <- round(minerals_g$phase_percent, 4)
+
+  out <- list("df" = minerals, "dfs" = minerals_g)
 
   return(out)
 }
@@ -39,9 +68,9 @@
 .qminerals2 <- function(x, xrd_lib, std, std_conc) {
 
   #Make sure x is ordered if there are more than 1 phases in the library
-  if (length(x) > 1) {
-    x <- x[order(names(x))]
-  }
+  #if (length(x) > 1) {
+  #  x <- x[order(names(x))]
+  #}
 
   #Get the name of the internal standard
   std_name <- xrd_lib$phases$phase_name[which(xrd_lib$phases$phase_id == std)]
@@ -71,10 +100,23 @@
   #Restrict the xrd library to phases within the names of X
   minerals <- minerals[which(minerals$phase_id %in% names(x)),]
 
+
+
   #Order to the same as fpf_pc
-  if (length(x) > 1) {
-    minerals <- minerals[order(minerals$phase_id),]
+  #if (length(x) > 1) {
+  #  minerals <- minerals[order(minerals$phase_id),]
+  #}
+
+  #make sure the minerals data frame is in the same order as x
+  minerals <- minerals[match(names(x), minerals$phase_id),]
+
+
+  if(!identical(names(x), minerals$phase_id)) {
+
+    stop("The phase ID's for quantification do not match the names of the scaling coefficients.")
+
   }
+
 
   min_percent <- c()
 
