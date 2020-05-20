0c052018e95316fc016a9384c50ea2b01fc93f1b
Benjamin Butler <benjamin.butler@hutton.ac.uk>
2020-05-20 14:41:49 +0100

    Reference and force selections no longer alphabetically ordered

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -323,8 +323,8 @@ shiny::shinyServer(function(input, output, session) {
                          "PHASE IDs" = as.list(paste0(filedata3()[[3]][[2]], ": ", filedata3()[[3]][[1]])))
 
       #Order alphabetically
-      refs_force[[1]] <- refs_force[[1]][order(unlist(refs_force[[1]]))]
-      refs_force[[2]] <- refs_force[[2]][order(unlist(refs_force[[2]]))]
+      #refs_force[[1]] <- refs_force[[1]][order(unlist(refs_force[[1]]))]
+      #refs_force[[2]] <- refs_force[[2]][order(unlist(refs_force[[2]]))]
 
       return(shiny::selectInput("selectFORCE",
                                 label = NULL,
@@ -340,8 +340,8 @@ shiny::shinyServer(function(input, output, session) {
         refs_choices <- list("PHASE_NAMES" = as.list(paste0(filedata3()[[3]][[2]])),
                              "PHASE IDs" = as.list(paste0(filedata3()[[3]][[2]], ": ", filedata3()[[3]][[1]])))
         #Order alphabetically
-        refs_choices[[1]] <- refs_choices[[1]][order(unlist(refs_choices[[1]]))]
-        refs_choices[[2]] <- refs_choices[[2]][order(unlist(refs_choices[[2]]))]
+        #refs_choices[[1]] <- refs_choices[[1]][order(unlist(refs_choices[[1]]))]
+        #refs_choices[[2]] <- refs_choices[[2]][order(unlist(refs_choices[[2]]))]
 
         return(shiny::updateSelectInput(session, "selectPHASES_fps",
                                         choices = refs_choices,
